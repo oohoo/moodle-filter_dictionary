7c9ed5ffde279daace9b5f9a8ef00004f781a908
patrickthibaudeau <patrick.thibaudeau@ualberta.ca>
2012-11-15 11:37:44 -0700

    Added version file

---
+++ version.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * *************************************************************************
+ * *                           Pop-up Dictionary                          **
+ * *************************************************************************
+ * @package     block                                                     **
+ * @subpackage  dictionary                                                **
+ * @name        dictionary                                                **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Patrick Thibaudeau                                        **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+$plugin->version = 2012111500;
+$plugin->requires = 2010112400;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.1 (Build: 2012111500)';
+$plugin->component = 'filter_dictionary';
+$plugin->cron = 0;
+?>
