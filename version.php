26f98560ebc49856c2fa95333e3b044d0d0be6e0
Nicolas Bretin <bretin@ualberta.ca>
2014-04-02 09:22:16 -0600

    Upgrade for moodle 2.6

--- version.php
+++ version.php
@@ -13,9 +13,9 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
  * *************************************************************************
  * ************************************************************************ */
-$plugin->version = 2013061800;
+$plugin->version = 2014040200;
 $plugin->requires = 2012120300;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.1.0 (Build: 2013061800)';
+$plugin->release = '1.2.0 (Build: 2014040200)';
 $plugin->component = 'filter_dictionary';
 $plugin->cron = 0;
