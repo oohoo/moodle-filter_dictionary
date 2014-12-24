8a9ce30c99ed6e6b91f31157d7f95734b8c1ebce
patrick <patrick@patrick-VirtualBox>
2014-12-24 09:52:12 -0500

    Updated version

--- version.php
+++ version.php
@@ -13,9 +13,9 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
  * *************************************************************************
  * ************************************************************************ */
-$plugin->version = 2014040200;
+$plugin->version = 2014122400;
 $plugin->requires = 2012120300;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.2.0 (Build: 2014040200)';
+$plugin->release = '1.3.0 (Build: 2014122400)';
 $plugin->component = 'filter_dictionary';
 $plugin->cron = 0;
